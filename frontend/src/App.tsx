396ebb90d0267ee8e4cc942119810a1dfdc86d63
Léon <leon.gallet@gmail.com>
2025-07-13 16:35:41 +0200

    test jenkins

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -36,7 +36,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMRRRRRRRRRRRRR
         </p>
       </div>
       <p className="read-the-docs">
